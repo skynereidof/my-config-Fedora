438ec6988f244fdc942723b8de8447837c8d0c1b
skynereidof <63476569+skynereidof@users.noreply.github.com>
2024-10-04 12:52:59 +0200

    amdrx570.sh

---
+++ sky/deckboard/.screenlayout/amdrx570.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1280x1024 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output HDMI-A-0 --primary --mode 1280x1024 --pos 1280x0 --rotate normal --output HDMI-A-1 --mode 1280x1024 --pos 2560x0 --rotate normal --output DVI-D-0 --off
